26e941058026387a9126c896ed88689ba8a7250b
TheVitik <lokplay2003@gmail.com>
2022-11-10 19:25:04 +0200

    Create models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,19 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model
+class Category extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
 
     protected $fillable = [
-        'name',
+        'name'
     ];
 }
